580ae548118a4395d89cc67ccddbc9af01e31612
ali zokaei <ali.zokaei.1367@gmail.com>
2024-08-17 01:29:55 +0330

    save me

--- components/PlayerMain/index.vue
+++ components/PlayerMain/index.vue
@@ -1,16 +1,36 @@
 <script setup>
-    import storeSimple from "@/store/storeSimple"
-    import DarkBackground from "@/components/DarkBackground"
+import storeSimple from "@/store/storeSimple"
+import DarkBackground from "@/components/DarkBackground"
 </script>
 <template>
     <div class="PlayerMain">
         <div class="main-container">
-           red
+            <div class="back-img">
+
+            </div>
         </div>
     </div>
 </template>
-<style>
-.main-container{
-    background-color: red;
+<style lang="scss">
+.PlayerMain{
+    height: 100vh;
+    .main-container {
+        height: 100%;
+        width: 100%;
+    position: relative;
+
+    .back-img {
+        position: absolute;
+        background-position: center;
+        background-size: cover;
+        width: 100%;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        background-image: url("https://vmusic.ir/wp-content/uploads/2023/05/Oneheart-searching-for-you-2023.jpg")
+    }
 }
+}
+
 </style>
