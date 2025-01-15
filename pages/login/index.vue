d0843ea3afb80d310bd7d35244039f5bc099031f
Ali Zokaei <ali.zokaei.arnitex@gmail.com>
2025-01-15 15:44:04 +0330

    hel test soundcloud

---
+++ pages/login/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+const fuck = ref(process.env.SUPABASE_KEY)
+
+</script>
+
+<template>
+  <div>
+    login paaaaaaage{{ fuck }}
+  </div>
+
+</template> 
--- pages/test/index.vue
+++ pages/test/index.vue
@@ -2,16 +2,27 @@
   hello login
   <button @click="postData()" class="pt-10">postData</button>
   <button @click="updateLiveMusic(objectToInsertLive)" class="pt-10">post muuuuusic</button>
-
+  <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay"
+    src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1848340500&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
+  </iframe>
+  <div
+    style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;">
+    <a href="https://soundcloud.com/electrojunkiee" title="Electro Junkiee" target="_blank"
+      style="color: #cccccc; text-decoration: none;">Electro Junkiee</a> · <a
+      href="https://soundcloud.com/electrojunkiee/techno-mix-122-cute-evil-mixed-by-ej"
+      title="TECHNO MIX #122 | Cute Evil | Mixed by EJ" target="_blank"
+      style="color: #cccccc; text-decoration: none;">TECHNO MIX #122 | Cute Evil | Mixed by EJ
+    </a>
+  </div>
 
   <div class="pt-8">
     <div>fuuuuuuuuuuuuuuuuuuuck</div>
     <div>
-    <h1>Set Future Time Alert</h1>
-    <input v-model="inputTime" placeholder="Enter time (HH:MM:SS)" />
-    <button @click="setFutureTime">Set Alert</button>
-    <p v-if="futureTimeString">Alert set for: {{ futureTimeString }}</p>
-  </div>
+      <h1>Set Future Time Alert</h1>
+      <input v-model="inputTime" placeholder="Enter time (HH:MM:SS)" />
+      <button @click="setFutureTime">Set Alert</button>
+      <p v-if="futureTimeString">Alert set for: {{ futureTimeString }}</p>
+    </div>
   </div>
 </template>
 
@@ -22,7 +33,7 @@ import { useAPI } from '~/composables/useAPI'
 import { useMusicAPI } from '~/composables/useMusicAPI'
 
 const { supabase } = useSupabase()
-const { getLiveMusic,updateLiveMusic } = useMusicAPI()
+const { getLiveMusic, updateLiveMusic } = useMusicAPI()
 
 // const { getData } = useAPI()
 // const { data, pending, error } = await getData('https://jsonplaceholder.typicode.com/posts')
