2e0ecc6d28dbeb9189f8cdeaf4ef20b9e1b79b65
freemanzok <ali.zokaei.1367@gmail.com>
2025-04-02 21:10:44 +0330

    FUCK YES META TAG

--- pages/index.vue
+++ pages/index.vue
@@ -7,3 +7,16 @@
 
 
 </template>
+<script setup>
+import { useHead } from '@vueuse/head'
+
+useHead({
+  title: 'DANCE BABY RADIO',
+  meta: [
+    {
+      name: 'description',
+      content:  "Listen to Beauties and Dance"
+    }
+  ]
+})
+</script>
