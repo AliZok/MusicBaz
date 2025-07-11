5e5fbd664a66b5d2030bee4e7d0dc5f477e71bce
freemanzok <ali.zokaei.1367@gmail.com>
2025-07-11 12:54:39 +0330

    save

--- pages/login.vue
+++ pages/login.vue
@@ -1,6 +1,16 @@
 <template>
-  hello login
-  <button @click="insertData()" class="pt-10">insertData</button>
+  <div class="mb-10">
+    hello world
+  </div>
+  <button class="mb-10" @click='postData("http://localhost:4000/api/auth/request-otp",{email:"ali.zokaei.1367@gmail.com"})'>insertData</button>
+  <div class="mb-10">
+    <button @click="createUser()">create one user</button>
+  </div>
+    <div class="mb-10">
+    <button @click="getUsers()">get all users</button>
+  </div>
+  
+  
 </template>
 
 <script setup>
@@ -16,7 +26,23 @@ const {
   loadingDeleteApi
 } = useAPI()
 
+function createUser(){
+  alert("fuck this system")
+  const firstUser = {
+    username:"FREEMANZOK",
+    email:"ali.zokaei.1367@gmail.com",
+    password:"this is a pass",
+    mobile:"09124888723",
+  }
+  postData("http://localhost:4000/api/auth/register",firstUser)
+}
+
+function getUsers(){
+
+  getData("http://localhost:4000/api/users")
+}
+
 onMounted(async () => {
-  postData("http://localhost:4000/api/auth/request-otp",{email:"ali.zokaei.1367@gmail.com"})
+  
 })
 </script>
