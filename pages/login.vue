dfbb3c70e4fa4a0631a2678d0f778f7051c93ffb
freemanzok <ali.zokaei.1367@gmail.com>
2025-07-12 15:39:18 +0330

    save first step login

--- pages/login.vue
+++ pages/login.vue
@@ -1,11 +1,17 @@
 <template>
-  <button class="mb-10" @click='postData("http://localhost:4000/api/auth/request-otp",{email:"ali.zokaei.1367@gmail.com"})'>insertData</button>
-  <div class="mb-10">
-    <button @click="createUser()">create one user</button>
-  </div>
+  <div class="relative">
+     <button class="mb-10" @click='postData("http://localhost:4000/api/auth/request-otp",{email:"ali.zokaei.1367@gmail.com"})'>insertData</button>
     <div class="mb-10">
-    <button @click="getUsers()">get all users</button>
+      <button @click="createUser()">create one user</button>
+    </div>
+    <div class="mb-10">
+      <button @click="getUsers()">get all users</button>
+    </div>
+    <div class="back-img"
+                :style="`background-image: url(${'images/background-dance-1.jpg'})`">
   </div>
+  </div>
+ 
   
 </template>
 
@@ -47,3 +53,18 @@ onMounted(async () => {
   
 })
 </script>
+
+<style scoped>
+ .back-img {
+            position: absolute;
+            background-position: center;
+            background-size: cover;
+            width: 100%;
+            top: 0;
+            bottom: 0;
+            left: 0;
+            right: 0;
+            filter: blur(5px);
+            z-index: 0;
+        }
+</style>
